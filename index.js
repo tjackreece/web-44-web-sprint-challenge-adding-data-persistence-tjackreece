bbc27245b627fb9000c79c590bdbfabdc49a7f67
TIMOTHY T JACKREECE <73547670+tjackreece@users.noreply.github.com>
2021-08-13 14:09:59 -0400

    mvp done codegrade messy test passed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// start your server here
+require("dotenv").config();
+const server = require("./api/server.js");
+
+const { PORT } = require("./config");
+
+server.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}...`);
+});
+// changes
